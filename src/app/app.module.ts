f70571a0aee72ed98ba2cd863c510656aca309cf
juan david   torres muñoz <juanda@MacBook-juanda.local>
2018-06-15 12:38:27 -0500

    globals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './globals/menu/menu.component';
+import { HeaderComponent } from './globals/header/header.component';
+import { CanastaComponent } from './globals/canasta/canasta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    CanastaComponent
   ],
   imports: [
     BrowserModule
